01d2a01ef2ec10ce5d91d6a291a3243a770d0ece
cansev <mee.cansev@gmail.com>
2023-08-22 16:34:33 +0300

    feat: ModalIds are captured and assigned to the function to create separate click events

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
+/* const keremac = (modalName) => {
+  document.getElementById(modalName).classList.remove("hidden");
+}; */
+
+const modalList = document.querySelector(".modal-list");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-const openModal = function () {
+/* const openModal = function () {
   modal.classList.remove("hidden");
 
   overlay.classList.remove("hidden");
-};
+}; */
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", openModal);
+/* for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal); */
 
-/* btnCloseModal.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-
-overlay.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-}); */
-
-const closeModal = function () {
+/* const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
-
-btnCloseModal.addEventListener("click", closeModal);
+ */
+/* btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape");
   if (!modal.classList.contains(".hidden")) {
     closeModal();
+  }
+}); */
+function showModal(modalId) {
+  console.log(modalId);
+  document.getElementById(modalId).classList.remove("hidden");
+}
+function closeModal(modalId) {
+  document.getElementById(modalId).classList.add("hidden");
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape");
+  if (!modalList.classList.contains(".hidden")) {
+    console.log(modalList.children);
+    modalList.child;
   }
 });
