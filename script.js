f9671ca64ffa71bdcb6ecc96aeca8121f8d2f60c
cansev <mee.cansev@gmail.com>
2023-08-24 21:54:04 +0300

    feat: added closing on click of outer div

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
 "use strict";
-
-/* const keremac = (modalName) => {
-  document.getElementById(modalName).classList.remove("hidden");
-}; */
-
 const modalList = document.querySelector(".modal-list");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
 
-/* const openModal = function () {
-  modal.classList.remove("hidden");
-
-  overlay.classList.remove("hidden");
-}; */
-
-/* for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", openModal); */
-
-/* const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
- */
-/* btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape");
-  if (!modal.classList.contains(".hidden")) {
-    closeModal();
-  }
-}); */
 function showModal(modalId) {
   console.log(modalId);
   document.getElementById(modalId).classList.remove("hidden");
 }
+
 function closeModal(modalId) {
   document.getElementById(modalId).classList.add("hidden");
 }
@@ -46,4 +20,20 @@ document.addEventListener("keydown", function (e) {
     if (!modalList.children[i].classList.contains(".hidden")) {
       modalList.children[i].classList.add("hidden");
     }
+});
+
+document.addEventListener("click", function (e) {
+  if (
+    !(
+      e.target.classList.contains("show-modal") ||
+      e.target.classList.contains("modal") ||
+      e.target.classList.contains("modal-content")
+    )
+  ) {
+    for (let i = 0; i < modalList.children.length; i++) {
+      if (!modalList.children[i].classList.contains("hidden")) {
+        modalList.children[i].classList.add("hidden");
+      }
+    }
+  }
 });
